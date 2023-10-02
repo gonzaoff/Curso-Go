5c44a812e83a347266ce1a3c1f664310127b904e
Gonzalo <sibolichgonzalo@gmail.com>
2023-10-01 21:30:07 -0300

    Inicio del curso de GoLand

---
+++ hello_world.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	//hola mundo
+	fmt.Println("Hola,Go")
+
+	/*
+		fmt.Println("Hola,Go")
+		fmt.Println("Hola,Go")
+		fmt.Println("Hola,Go")
+		fmt.Println("Hola,Go")
+	*/
+	fmt.Println("Hola,Go")
+
+}
